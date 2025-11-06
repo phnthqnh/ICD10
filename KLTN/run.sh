623fa5ab287e7ac3820a52c9655f8cf713249966
phnthqnh <qphan204@gmail.com>
2025-11-06 22:14:02 +0700

    bash run websocket

--- KLTN/run.sh
+++ KLTN/run.sh
@@ -12,4 +12,6 @@ python manage.py migrate ICD10 --noinput
 
 echo "================================ ✅ DONE MIGRATES ======================================="
 
-python manage.py runserver 0.0.0.0:8000
+# python manage.py runserver 0.0.0.0:8000
+
+daphne -b 0.0.0.0 -p 8000 KLTN.asgi:application
