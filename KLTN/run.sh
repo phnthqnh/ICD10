46b4951ac6232aff887bb8f7ad44fa08495142bd
phnthqnh <qphan204@gmail.com>
2025-12-27 23:24:17 +0700

    update: user uvicorn

--- KLTN/run.sh
+++ KLTN/run.sh
@@ -14,4 +14,6 @@ echo "================================ ✅ DONE MIGRATES =======================
 
 # python manage.py runserver 0.0.0.0:8000
 
-daphne -b 0.0.0.0 -p 8000 KLTN.asgi:application
+# daphne -b 0.0.0.0 -p 8000 KLTN.asgi:application
+uvicorn KLTN.asgi:application --host 0.0.0.0 --port 8000
+
